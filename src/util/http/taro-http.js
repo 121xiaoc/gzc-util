4e43e25f27a195dfcc5fd2bbd2d616a4279f9ce8
葛振超 <gezhenchao@hexiaoxiang.com>
2020-03-28 16:10:02 +0800

    feat:  +taro-http

--- src/util/http/taro-http.js
+++ src/util/http/taro-http.js
@@ -21,8 +21,54 @@ class Http {
     return this.request('GET', requestHandler)
   }
 
-  request() {
-    const { url, params, headers } = requestHandler
+  request(method = 'GET') {
+    const { 
+      url,
+      data,
+      header = { 'content-type': 'application/json' }
+    } = requestHandler
+    method = method.toUpperCase();
+    // 通过环境设置请求的参数
+    // const baseUrl = baseUrlList[env] || baseUrlList.development;
+    // url = /^http/g.test(url) ? url : baseUrl + url;
+    return new Promise((resolve, reject) => {
+      const requestTask = Taro.request({ url, method, header, data });
+      requestTask.then(res => {
+        const { data, statusCode } = res
+        if(/^2/g.test(statusCode)) {
+          // resolve(data)
+          return Promise.resolve(res)
+        }
+        function httpErrorMsgAction(){
+          if (/^3/g.test(statusCodeStr)) return '重定向错误';
+          if (/^4/g.test(statusCodeStr)) return '客户端错误';
+          if (/^5/g.test(statusCodeStr)) return '服务器错误';
+          return '未知错误'
+        }
+        return Promise.reject(httpErrorMsgAction())
+      }).then(res => {
+        const { code } = res;
+        const errMsg = res.errMsg || res.msg
+        if (code === 20000 || code === 200) {
+          resolve(data)
+        } else if (code === 40003) {
+          // 登录
+        } else {
+          Promise.reject(errMsg)
+        }
+      }, httpErrMsg => {
+        // 只捕获 http 错误状态
+        Taro.showModal({
+          title: '错误提示',
+          content: JSON.stringify(httpErrMsg)
+        }).then(res => {
+          if (res.confirm) {
+            Http(httpPrams);
+          }
+          reject()
+        });
+      })
+    })
   }
 }
 
